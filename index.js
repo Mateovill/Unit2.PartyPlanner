c8d9fb2ce7a2234d2aea0fc60f686f4c3591677f
Mateo Villezca-Rivera <mateojvr@Mateos-iMac.local>
2024-03-25 22:09:20 -0600

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const baseURL='https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events';
+//the baseURl is used to communicate with the server to access the api 
 
 
 async function logAsync (func){
     const result = await func();
     console.log(result);
 }
+const state ={
+    events: [],
+    messages: 'This object is n ot in the database!',
+    isError: false
+};
+
 //display all the parties using a fetch command
 async function getEvent (){
     const response = await fetch(`${baseURL}`); //create a response variable to fetch the data from url
@@ -13,42 +20,55 @@ async function getEvent (){
     if(!json.success){      //if the json is not successful
         throw new Error(json.error);    //then throw an error to let us know
     }
-    return json.data;   //if we did receive then we would return this 
+  state.events = [...json.data];
+
+render();  
 }
-logAsync(getEvent);    //this will invoke the function
 
 //create a party
 
 
 //render party to broswer 
 function addEventToScreen(r){
-const eventElement = document.querySelector('events'); //gets information from the document for the events
+const eventElement = document.getElementById('events'); //gets information from the document for the events
 const elem = document.createElement('div');
 elem.classList.add('event')
+elem.setAttribute('data-id', r.id); //this will add an id to all the party elements from the html on the browser
 
-const idElem = document.querySelector('div');
+
+const idElem = document.createElement('div');
 idElem.classList.add('id');
 idElem.append(r.id);
 
-const nameElem = document.querySelector('div');
+const nameElem = document.createElement('div');
 nameElem.classList.add('name');
 nameElem.append(r.id);
 
-const dateElem = document.querySelector('div');
+const dateElem = document.createElement('div');
 dateElem.classList.add('date');
 dateElem.append(r.date);
 
-const locationElem = document.querySelector('div');
+const locationElem = document.createElement('div');
 locationElem.classList.add('location');
 locationElem.append(r.location);
 
-const descriptionElem = document.querySelector('div');
+const descriptionElem = document.createElement('div');
 descriptionElem.classList.add('description');
 descriptionElem.append(r.description)
 
-const cohortIdElem =document.querySelector('div');
+const cohortIdElem =document.createElement('div');
 cohortIdElem.classList.add('cohortId');
 cohortIdElem.append(r.cohortId);
+
+const delButtonElm = document.createElement('button') //we created a button that we can now use
+const buttonText =document.createElement('Delete');  //We will create a text node, 
+delButtonElm.appendChild(buttonText); //this gives the delete button text to show that they have 
+
+delButtonElem.addEventListener('click', async (done) => {
+    const selectedEvent = done.target.closets('.event');
+    const id = selectedEvent.dataset.id;
+    const result = await deleteEvent(id)
+})
 
 elem.append(idElem);
 elem.append(nameElem);
@@ -56,17 +76,14 @@ elem.append(dateElem);
 elem.append(locationElem);
 elem.append(cohortIdElem);
 elem.append(descriptionElem);
+elem.append(delButtonElem);
 
 eventElement.append(elem);
 }
 
-document.addEventListener('DOMContentLoaded', async ()=> {
-    const events = await getEvent();
-events.forEach(r => {
-    addEventToScreen(r);
-});
+//eventElement.append(elem);
 
-const form = document.querySelector('eventForm');
+const form = document.getElementById('eventForm');
 form.addEventListener('submit', async(event) =>{
     event.preventDefault();
 
@@ -92,11 +109,29 @@ form.addEventListener('submit', async(event) =>{
     }catch(err){
         console.log(err);
     }
-})
 });
 
-//create a delete button to remove a party from the list 
 
+//create a delete button to remove a party from the list 
+//every button needs an event listener and needs a refrence to the html and give it a call back function to give it some functionality
+function render(){
+    state.events.forEach((r) => {
+        addEventToScreen(r);
+    });
+}
+async function init(){
+    await getEvent();
+}
+init();
+
+async function deleteEvent(id){
+    try {
+        const response = await fetch(`${baseURL}${id}`, {
+            method: 'delete',
+        })
+    }
+}
+init()
 //Dynamically have button add p parties created in the form
 
-// 
+// the changes that we made content loaded - i do not want to trigger the event
